46e12f36f9106b4d41d24a71b76aa4c44d6b522f
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-02-29 10:31:20 +0200

    Add files via upload

---
+++ Hello_World_GUI_2024/src/hello_world_gui_2024/Hello_World_GUI_2024.java
@@ -0,0 +1,16 @@
+
+package hello_world_gui_2024;
+
+//import statement allows access to built-in libr & packages
+//import statement sits between the package name & class name
+//import javax.swing.*; (alternate import statement with a wildcard symbol)
+
+import javax.swing.JOptionPane;
+
+public class Hello_World_GUI_2024 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World GUI");
+    }
+    
+}
