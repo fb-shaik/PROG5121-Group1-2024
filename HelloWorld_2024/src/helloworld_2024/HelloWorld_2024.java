6f0515ace00348bab13aa898416deaeff5e0c7bf
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-02-29 10:30:11 +0200

    Add files via upload

---
+++ HelloWorld_2024/src/helloworld_2024/HelloWorld_2024.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld_2024;
+
+/**
+ *
+ * @author fshaik
+ */
+public class HelloWorld_2024 {
+
+    //this is a main method; every instruction that I want my app to do has to be placed within 
+    //the main method curly braces
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
