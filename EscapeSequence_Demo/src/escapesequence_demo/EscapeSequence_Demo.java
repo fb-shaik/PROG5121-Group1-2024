373b8b2e7081ffb54cd02e20dffc8272c3b81abf
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-03-11 01:08:43 +0200

    Add files via upload

---
+++ EscapeSequence_Demo/src/escapesequence_demo/EscapeSequence_Demo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package escapesequence_demo;
+
+/**
+ *
+ * @author fshaik
+ */
+public class EscapeSequence_Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        //print a new line for the text below: \n
+        System.out.println("Hello\nWorld!");
+        //print a tab space in the text
+        System.out.println("Hello\tWorld!");
+        // print a set of " " 
+        System.out.println("They said: \"Hello!\"");
+        //print a single ''
+        System.out.println("'Be Happy'");
+    }
+    
+}
